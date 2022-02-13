41d9021b733a424c4eef905b762959686c894e02
Odo Luo <luo14938@gmail.com>
2022-02-13 14:43:29 +0100

    Parallel getting folder and normalize images between 0 and 1

--- getDim.m
+++ getDim.m
@@ -1,20 +1,46 @@
 directory = uigetdir;
+tic
+% load files into variable files and get its count
 files = dir (strcat(directory,'\*.nii.gz'));
-L = length (files);
+files_count= length (files);
 
-x = ones(1,L);
-y = ones(1,L);
-z = ones(1,L);
+% generating arrays for each dimension
+dim_x = ones(1,files_count);
+dim_y = ones(1,files_count);
+dim_z = ones(1,files_count);
 
-for i=1:L
+% go through each file
+
+local_min=zeros(files_count);
+local_max=zeros(files_count);
+parfor i=1:files_count
+    file = strcat(directory,'\',files(i).name);
+    image = load_nii(file).img;
+    
+    % Set size of images
+    s=size(image);
+    dim_x(i)= s(1);
+    dim_y(i)= s(2);
+    dim_z(i)= s(3);
+    % Search for global min and max
+    %%
+    local_max(i)= max(image,[],'all');
+    local_min(i)= min(image,[],'all');
+
+end
+global_max = max(local_max);
+global_min = min(local_min);
+global_max=global_max(1);
+global_min=global_min(1);
+
+parfor i=1:files_count
     file = strcat(directory,'\',files(i).name);
     image = load_nii(file);
-    s=size(image.img);
-    x(i)= s(1);
-    y(i)= s(2);
-    z(i)= s(3);
+    image.img = mat2gray(image.img,[global_min,global_max]);
+    save_nii(image, strcat('E:\git\maskRCNN\norm\',files(i).name));
 end
 
-disp(mean(x))
-disp(mean(y))
-disp(mean(z))
+toc
+disp(mean(dim_x))
+disp(mean(dim_y))
+disp(mean(dim_z))
