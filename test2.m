f541aee8418de0dad03b8ff341bf6503a6ce362d
Odo Luo <luo14938@gmail.com>
2022-04-11 19:53:45 +0200

    datastore

--- test2.m
+++ test2.m
@@ -1,16 +1,42 @@
-m = [   0, 0, 0,1,1,1,2 ;
-    0,0,1,1,1,1,2;
-    0,1,1,1,1,1,0;
-    0,0,0,1,1,0,0;
-    1,1,0,1,1,1,0] % Last row has 2 contiguous regions instead of 1.
-mb = m == 1
-[rows, columns] = size(mb);
-for row = 1 : rows
-    % Make measurements of all lines of 1's.
-    props = regionprops(mb(row, :), 'Area');
-    % Extract all the lengths into a vector and then put into a cell.
-    R{row} = [props.Area];
+directory = uigetdir;
+imds = imageDatastore(directory,'FileExtensions','.gz',"ReadFcn",@fcn);
+
+function ds = fcn(data)
+    filename = strsplit(data,'\');
+    labelFolder= uigetdir;
+    labelPath= strcat(labelFolder,'\',char(filename{end}));
+    label =load_nii(labelPath);
+    ct=load_nii(data);
+    image=ct.img;
+    [xSize,ySize,zSize] = size(image);
+    for z = 1:zSize
+        boxes=[];
+        categorical=[];
+
+        i = squeeze(label.img(1:(xSize/2),:,z))==1;
+        mask1 = i;
+        mask1(1+(xSize/2):xSize,:)=0;
+        [xCoordinate,yCoordinate,boxWidth,boxHeight] = getCoordinates(i);
+
+        if boxWidth  ~= 0 && boxHeight ~= 0 
+            boxes=[xCoordinate yCoordinate boxWidth boxHeight];
+%             categorical=['kidney'];
+        end
+
+        i = squeeze(label.img((1+(xSize/2):xSize),:,z))==1;
+        mask2(1:(xSize/2),:)=0;
+        mask2(1+(xSize/2):xSize,1:ySize)= i;
+        [xCoordinate,yCoordinate,boxWidth,boxHeight] = getCoordinates(i);
+        if boxWidth  ~= 0 && boxHeight ~= 0 
+            boxes= [boxes; xCoordinate yCoordinate boxWidth boxHeight];
+%             categorical=[categorical;'kidney']
+%             ds{z,2}=boxes;
+%             ds{z,3}= categorical;
+        end
+
+        masks(:,:,1)=mask1;
+        masks(:,:,2)=mask2;
+%       ds{z,4}=masks;
+        ds{z,1}=image(:,:,z);
+    end
 end
-
-
-out = cellfun(@sum,R);
--- tictoctest.m
+++ tictoctest.m
@@ -1,10 +1,20 @@
+% x=niftiread("E:\git\maskRCNN\testImg\case_00173.nii");
+% directory = uigetdir;
+
+ctds = imageDatastore("E:\git\maskRCNN\testImg\case_00173.nii.gz", ...
+    'FileExtensions','.gz','ReadFcn',@ct);
+
+blocks= block('E:\git\maskRCNN\testImg\case_00173.nii.gz');
+
+lables = imageDatastore("E:\git\maskRCNN\labelsTr\case_00173.nii.gz", ...
+    'FileExtensions','.gz','ReadFcn',@ct);
+
+ds= combine(ctds,blocks,lables);
+
 trainClassNames = {'kidney'};
 numClasses = length(trainClassNames);
 imageSizeTrain = [512 512 3];
 
-directory = uigetdir;
-
-ds=fileDatastore(directory,"ReadFcn",@fcn);
 net = maskrcnn("resnet50-coco",trainClassNames,InputSize=imageSizeTrain);
 options = trainingOptions("sgdm", ...
     InitialLearnRate=0.001, ...
@@ -27,7 +37,7 @@ if doTraining
     save("trainedMaskRCNN-"+modelDateTime+".mat","net");
 end
 
-function ds = fcn(data)
+function blockds = block(data)
     filename = strsplit(data,'\');
     labelFolder= uigetdir;
     labelPath= strcat(labelFolder,'\',char(filename{end}));
@@ -35,37 +45,52 @@ function ds = fcn(data)
     ct=load_nii(data);
     image=ct.img;
     [xSize,ySize,zSize] = size(image);
+    box=cell(zSize,2);
     for z = 1:zSize
         boxes=[];
-        categorical=[];
-
+        cat={};
         i = squeeze(label.img(1:(xSize/2),:,z))==1;
-        mask1 = i;
-        mask1(1+(xSize/2):xSize,:)=0;
         [xCoordinate,yCoordinate,boxWidth,boxHeight] = getCoordinates(i);
 
         if boxWidth  ~= 0 && boxHeight ~= 0 
-            boxes=[xCoordinate yCoordinate boxWidth boxHeight];
-            categorical=['kidney'];
+            boxes(1,:)=[xCoordinate yCoordinate boxWidth boxHeight];
         end
 
         i = squeeze(label.img((1+(xSize/2):xSize),:,z))==1;
-        mask2(1:(xSize/2),:)=0;
-        mask2(1+(xSize/2):xSize,1:ySize)= i;
         [xCoordinate,yCoordinate,boxWidth,boxHeight] = getCoordinates(i);
         if boxWidth  ~= 0 && boxHeight ~= 0 
-            boxes= [boxes; xCoordinate yCoordinate boxWidth boxHeight];
-            categorical=[categorical;'kidney']
-            ds{z,2}=boxes;
-            ds{z,3}= categorical;
+            [x,~]=size(boxes);
+            if(x==0)
+                boxes(1,:)=  [ xCoordinate yCoordinate boxWidth boxHeight];
+            else
+                boxes(2,:)= [ xCoordinate yCoordinate boxWidth boxHeight];
+            end
         end
-        if z == 56
-            display(z)
+        [x,~]=size(boxes);
+        if(x==1)
+            box{z,1}=boxes;
+            cat{1}='kidney';
+        elseif(x==2)
+            box{z,1}=boxes;
+            cat=cell(2,1);
+            cat{1}='kidney';
+            cat{2}='kidney';
+        elseif(x==0)
+            cat={};
         end
-        masks(:,:,1)=mask1;
-        masks(:,:,2)=mask2;
-        ds{z,4}=masks;
-        ds{z,1}=image(:,:,z);
+        box{z,2}=cat;
     end
+    table = cell2table(box,'VariableNames',{'Boxes','Labels'});
+    blockds= boxLabelDatastore(table);
 end
 
+function ds = ct(data)
+    ct=load_nii(data);
+    image=ct.img;
+    [~,~,zSize] = size(image);
+    for z = 1:zSize        
+        ds{z,:,:,1}=image(:,:,z);
+        ds{z,:,:,2}=0;
+        ds{z,:,:,3}=0;
+    end
+end
